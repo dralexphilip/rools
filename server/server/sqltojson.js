d365c4abf77c8a99993e42f37e8d4361ef805c40
dralexphilip <37393514+dralexphilip@users.noreply.github.com>
2022-08-01 23:26:58 +0530

    Updates

--- server/server/sqltojson.js
+++ server/server/sqltojson.js
@@ -27,19 +27,11 @@ function sqlToJson(sql) {
 }
 
 function processInsert(sql){
-
-    
-    
-    
-    let combinator = {"combinator": "or",
-                        "rules": []
-                    }
+   
     let select = {
                     "combinator": "and",
                     "rules": []
                 }
-
-    let rools = []
 
     var a = [], r = [];
     for(var i=0; i < sql.length; i++){
@@ -47,12 +39,13 @@ function processInsert(sql){
             a.push(i);
         }
         if(sql.charAt(i) == ')'){
-            if(sql.substring(a.pop()+1,i).includes('AND') || sql.substring(a.pop()+1,i).includes('OR'))
-                r.push(sql.substring(a.pop()+1,i));
+            let lowSql = sql.substring(a.pop()+1,i)
+            if(lowSql.includes('('))
+                r.push(lowSql);
         }
     }  
     
-        exp = sql.split('AND');
+    exp = sql.split('AND');
     
     
     for(var i = 0; i < exp.length; i++) {
@@ -62,60 +55,67 @@ function processInsert(sql){
                 exp[i].indexOf("(") + 1, 
                 exp[i].lastIndexOf(")")
             );
-            exp[i] = exp[i].split('OR');
-            for(var j = 0; j < exp[i].length; j++) {
-                combinator.rules.push(exp[i][j].trim())
-                for(var k = 0; k < combinator.rules.length; k++) {
-                    if(combinator.rules[k].toString().includes('IN', 0)){
-                        let temp = combinator.rules[k].toString().split("'").join("").split('IN')
-                        combinator.rules[k] = {}
-                        combinator.rules[k].field = temp[0].trim()
-                        combinator.rules[k].operator = 'in'
-                        combinator.rules[k].value = temp[1].trim().substring(temp[1].trim().indexOf("(") + 1,temp[1].trim().lastIndexOf(")")).trim().split(', ')
-                    }
-                    else if(combinator.rules[k].toString().includes('=', 0)){
-                        let temp = combinator.rules[k].split('=')
-                        combinator.rules[k] = {}
-                        combinator.rules[k].field = temp[0].trim()
-                        combinator.rules[k].operator = 'equal'
-                        combinator.rules[k].value = temp[1].trim().split("'").join("")
-                    }
-                }
-            }
-            
-            exp[i] = combinator
-            
+            exp[i] = exp[i].split('OR');  
+            exp[i] = processOperators(exp[i])            
         }
-
-        
+        else if(exp[i].includes('AND', 0)){
+            exp[i] = exp[i].substring(
+                exp[i].indexOf("(") + 1, 
+                exp[i].lastIndexOf(")")
+            );
+            exp[i] = exp[i].split('AND');  
+            exp[i] = processOperators(exp[i])            
+        }        
         select.rules.push(exp[i])
     }
-    for(var y = 0; y < select.rules.length; y++) {
-        if(select.rules[y].toString().includes('=', 0)){
-            let temp = select.rules[y].split('=')
-            select.rules[y] = {}
-            select.rules[y].field = temp[0].trim()
-            select.rules[y].operator = 'equal'
-            select.rules[y].value = temp[1].trim().split("'").join("")
+
+    
+    select.rules = processOperators(select.rules)
+
+    return select;
+}
+
+function processOperators(rules){
+    for(var y = 0; y < rules.length; y++) {
+        if(rules[y].toString().includes('=', 0)){
+            let temp = rules[y].split('=')
+            rules[y] = {}
+            rules[y].field = temp[0].trim()
+            rules[y].operator = 'equal'
+            rules[y].value = temp[1].trim().split("'").join("")
         }
-        else if(select.rules[y].toString().includes('NOT LIKE', 0)){
-            let temp = select.rules[y].split('NOT LIKE')
-            select.rules[y] = {}
+        else if(rules[y].toString().includes('NOT LIKE', 0)){
+            let temp = rules[y].split('NOT LIKE')
+            rules[y] = {}
             if(temp[0].trim().includes('COALESCE',0))
-                select.rules[y].field = temp[0].trim().replace('COALESCE(','coalesce_').split(',')[0]
-            select.rules[y].operator = 'not_contains'
-            select.rules[y].value = temp[1].trim().split("'").join("").split("%").join("")
+                rules[y].field = temp[0].trim().replace('COALESCE(','coalesce_').split(',')[0]
+            rules[y].operator = 'not_contains'
+            rules[y].value = temp[1].trim().split("'").join("").split("%").join("")
         }
-        else if(select.rules[y].toString().includes('IN', 0)){
-            let temp = select.rules[y].toString().split("'").join("").split('IN')
-            select.rules[y] = {}
-            select.rules[y].field = temp[0].trim()
-            select.rules[y].operator = 'in'
-            select.rules[y].value = temp[1].trim().substring(temp[1].trim().indexOf("(") + 1,temp[1].trim().lastIndexOf(")")).trim().split(', ')
+        else if(rules[y].toString().includes('LIKE', 0)){
+            let temp = rules[y].split('LIKE')
+            rules[y] = {}
+            if(temp[0].trim().includes('COALESCE',0))
+                rules[y].field = temp[0].trim().replace('COALESCE(','coalesce_').split(',')[0]
+            rules[y].operator = 'contains'
+            rules[y].value = temp[1].trim().split("'").join("").split("%").join("")
+        }
+        else if(rules[y].toString().includes('NOT IN', 0)){
+            let temp = rules[y].toString().split("'").join("").split('NOT IN')
+            rules[y] = {}
+            rules[y].field = temp[0].trim()
+            rules[y].operator = 'not in'
+            rules[y].value = temp[1].trim().substring(temp[1].trim().indexOf("(") + 1,temp[1].trim().lastIndexOf(")")).trim().split(', ')
+        }
+        else if(rules[y].toString().includes('IN', 0)){
+            let temp = rules[y].toString().split("'").join("").split('IN')
+            rules[y] = {}
+            rules[y].field = temp[0].trim()
+            rules[y].operator = 'in'
+            rules[y].value = temp[1].trim().substring(temp[1].trim().indexOf("(") + 1,temp[1].trim().lastIndexOf(")")).trim().split(', ')
         }
     }
-
-    return r;
+    return rules;
 }
 
 module.exports = { sqlToJson };
