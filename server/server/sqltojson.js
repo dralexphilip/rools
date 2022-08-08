1c533c468a4231d93f07bc46fa58d34fc819d909
dralexphilip <37393514+dralexphilip@users.noreply.github.com>
2022-08-08 12:09:19 +0530

    Updates

--- server/server/sqltojson.js
+++ server/server/sqltojson.js
@@ -9,15 +9,21 @@ function sqlToJson(sql) {
     let rools = []
     for(var y = 0; y < allRools.length; y++) {
         let rool = {}
+        
         if(allRools[y].toString().includes('insert', 0)){
-            let index = allRools[y].toString().indexOf("'")
-            rool.id = allRools[y].toString().trim().substring(allRools[y].toString().indexOf("'"), index+8).replace("'", "")
-            rool.description = rool.id
-            rool.insertSql = allRools[y].toString().trim()
-            rool.selectRules = processInsert(rool.insertSql.split('where')[1].toUpperCase())
+            let sqlContent = allRools[y].toString().trim().split('where')[1].toUpperCase().trim()
+            let maxD = maxDepth(sqlContent);
+            if(maxD<2){
+                let index = allRools[y].toString().indexOf("'")
+                rool.id = allRools[y].toString().trim().substring(allRools[y].toString().indexOf("'"), index+8).replace("'", "")
+                rool.description = rool.id
+                rool.insertSql = allRools[y].toString().trim()
+                rool.selectRules = processInsert(sqlContent)
+                rool.depth = maxD
+            }
         }
         else if(allRools[y].toString().includes('update', 0)){
-            rools[y-1].updateSql = allRools[y].toString().trim()
+            //rools[y-1].updateSql = allRools[y].toString().trim()
         }
         rools.push(rool)
     }
@@ -32,7 +38,7 @@ function processInsert(sql){
                     "combinator": "and",
                     "rules": []
                 }
-    sql = '('+sql.trim()+')'
+    sql = sql.trim()
     var a = [], r = [];
     for(var i=0; i < sql.length; i++){
         if(sql.charAt(i) == '('){
@@ -40,13 +46,28 @@ function processInsert(sql){
         }
         if(sql.charAt(i) == ')'){
             let lowSql = '('+sql.substring(a.pop()+1,i)+')'
-            //if(lowSql.includes('(')&&(lowSql.includes('AND')||lowSql.includes('OR'))){
-                if(lowSql.includes('(')&&lowSql.includes('IN'))
-                    lowSql.replace('(', '{').replace(')', '}')
+            let lowSql1 = '('+sql.substring(a.pop()+1,i)+')'
+            if(lowSql.includes('AND')||lowSql.includes('OR')){
+                //if(lowSql.includes('(')&&lowSql.includes('IN'))
+                    //lowSql.replace('(', '{').replace(')', '}')
+                    r.push(lowSql);
+                
+            }
+            else{
+                lowSql = lowSql.replace('(', '{').replace(')', '}')
                 r.push(lowSql);
-            //}
+                
+                //if(sql.includes(lowSql.substring(1,lowSql.length-1))){
+                    //sql = sql.replace(lowSql1, lowSql)
+                //    console.log(lowSql)
+                //}
+            }
         }
     }  
+
+    //r = r.filter(item => item.includes('AND', 0)||item.includes('OR', 0))
+    
+
     
     exp = sql.split('AND');
     
@@ -74,111 +95,133 @@ function processInsert(sql){
 
     
     rule.rules = processOperators(rule.rules)
+    rule.rules.forEach(ru => {
+        console.log(ru)
+        //rule.rules.push(processInsert(ru))
+    })
 
-    return breakdown(r[r.length-1])//shitshow(r[r.length-1]);
+    return rule//shitshow(r[r.length-1]);
 }
-
-function breakdown(str) {
-    var i = 0;
-    var trailingWhiteSpace = str[str.length - 1] === " ";
-    function main() {
-      var arr = [];
-      var startIndex = i;
-      function addWord() {
-        if (i-1 > startIndex) {
-          arr.push(str.slice(startIndex, i-1));
+function maxDepth(s){
+ 
+    let count = 0
+    let st = []
+    for(let i=0;i<s.length;i++){
+        if (s[i] == '('){
+            st.push(i) // pushing the bracket in the stack
         }
-      }
-      while (i < str.length) {
-        switch(str[i++]) {          
-            case " and ":
-                arr.push(main());
-                startIndex = i;
-                continue;
-            case " or ":
-                arr.push(main());
-                startIndex = i;
-                continue;
-            case "(":
-                arr.push(main());
-                startIndex = i;
-                continue;
-            case ")":
-                addWord();
-                return arr;
+        else if (s[i] == ')'){
+            if (count < st.length){
+                count = st.length
+            }
+            // keeping track of the parenthesis and storing
+            // it before removing it when it gets balanced
+            st.pop()
+            //console.log(st)
         }
-      }
-      if(!trailingWhiteSpace){
-        i = i + 1;
-        addWord();
-      }
-      return arr;
     }
-    return main();
-  }
-
-  function blah(string) {
-	string = string.replace(/\(/g, "[");
-	string = string.replace(/\)\s/g, "], ");
-	string = string.replace(/\)/g, "]");
-	string = string.replace(/\s+/, ", ");
-	string = "[" + string + "]";
-	string = string.replace(/[^\[\]\,\s]+/g, "\"$&\"");
-	string = string.replace(/" /g, "\", ");
-
-	return JSON.parse(string);
+        
+    return count
 }
-
-function processText(text) {
-    const levels = []
-    let depth = 0
-    for (const c of text) {
-      if (c === '(') depth++
-      if (depth >= levels.length) levels.push([])
-      levels[depth].push(c)
-      if (c === ')') depth--
-    }
-    return levels.map(level => level.join(''))
-  }
 
 function processOperators(rules){
     for(var y = 0; y < rules.length; y++) {
         if(rules[y].toString().includes('=', 0)){
             let temp = rules[y].split('=')
             rules[y] = {}
-            rules[y].field = temp[0].trim()
+            if(temp[0].trim().includes('COALESCE',0))
+                rules[y].field = temp[0].trim().replace('COALESCE(','coalesce_').split(',')[0]
+            else
+                rules[y].field = temp[0].trim()
             rules[y].operator = 'equal'
             rules[y].value = temp[1].trim().split("'").join("")
         }
-        else if(rules[y].toString().includes('NOT LIKE', 0)){
-            let temp = rules[y].split('NOT LIKE')
+        else if(rules[y].toString().includes(' NOT LIKE ', 0)){
+            let temp = rules[y].split(' NOT LIKE ')
+            let value = temp[1].trim().split("'").join("")
+            let begins_with = value.substring(0,1)
+            let ends_with = value.substring(value.length-1)
             rules[y] = {}
             if(temp[0].trim().includes('COALESCE',0))
                 rules[y].field = temp[0].trim().replace('COALESCE(','coalesce_').split(',')[0]
-            rules[y].operator = 'not_contains'
-            rules[y].value = temp[1].trim().split("'").join("").split("%").join("")
+            else
+                rules[y].field = temp[0].trim()
+            if(begins_with=='%'&&ends_with=='%')
+                rules[y].operator = 'not_contains'
+            else if(begins_with=='%'&&ends_with!='%')
+                rules[y].operator = 'does_not_end_with'
+            else if(begins_with!='%'&&ends_with=='%')
+                rules[y].operator = 'does_not_begin_with'
+            rules[y].value = value.split("%").join("")
         }
-        else if(rules[y].toString().includes('LIKE', 0)){
-            let temp = rules[y].split('LIKE')
+        else if(rules[y].toString().includes(' LIKE ', 0)){
+            let temp = rules[y].split(' LIKE ')
+            let value = temp[1].trim().split("'").join("")
+            let begins_with = value.substring(0,1)
+            let ends_with = value.substring(value.length-1)
             rules[y] = {}
             if(temp[0].trim().includes('COALESCE',0))
                 rules[y].field = temp[0].trim().replace('COALESCE(','coalesce_').split(',')[0]
-            rules[y].operator = 'contains'
-            rules[y].value = temp[1].trim().split("'").join("").split("%").join("")
+            else
+                rules[y].field = temp[0].trim()
+            if(begins_with=='%'&&ends_with=='%')
+                rules[y].operator = 'contains'
+            else if(begins_with=='%'&&ends_with!='%')
+                rules[y].operator = 'ends_with'
+            else if(begins_with!='%'&&ends_with=='%')
+                rules[y].operator = 'begins_with'
+            rules[y].value = value.split("%").join("")
         }
-        else if(rules[y].toString().includes('NOT IN', 0)){
-            let temp = rules[y].toString().split("'").join("").split('NOT IN')
+        else if(rules[y].toString().includes(' NOT IN ', 0)){
+            let temp = rules[y].toString().split("'").join("").split(' NOT IN ')
             rules[y] = {}
-            rules[y].field = temp[0].trim()
-            rules[y].operator = 'not in'
-            rules[y].value = temp[1].trim().substring(temp[1].trim().indexOf("(") + 1,temp[1].trim().lastIndexOf(")")).trim().split(', ')
+            rules[y].combinator = "or"
+            rules[y].rules = []
+            let values = temp[1].trim().substring(temp[1].trim().indexOf("(") + 1,temp[1].trim().lastIndexOf(")")).trim().split(',')
+            let field = temp[0].trim()
+            if(temp[0].trim().includes('COALESCE',0))
+                field = temp[0].trim().replace('COALESCE(','coalesce_').split(',')[0]
+            else
+                field = temp[0].trim()
+            let op = 'not_equal'
+            values.forEach(v => {
+                let rule = {}
+                rule.field = field
+                rule.operator = op
+                rule.value = v
+                rules[y].rules.push(rule)
+            });
+            
         }
-        else if(rules[y].toString().includes('IN', 0)){
-            let temp = rules[y].toString().split("'").join("").split('IN')
+        else if(rules[y].toString().includes(' IN ', 0)){
+            let temp = rules[y].toString().split("'").join("").split(' IN ')
             rules[y] = {}
-            rules[y].field = temp[0].trim()
-            rules[y].operator = 'in'
-            rules[y].value = temp[1].trim().substring(temp[1].trim().indexOf("(") + 1,temp[1].trim().lastIndexOf(")")).trim().split(', ')
+            rules[y].combinator = "or"
+            rules[y].rules = []
+            let values = temp[1].trim().substring(temp[1].trim().indexOf("(") + 1,temp[1].trim().lastIndexOf(")")).trim().split(',')
+            let field = temp[0].trim()
+            if(temp[0].trim().includes('COALESCE',0))
+                field = temp[0].trim().replace('COALESCE(','coalesce_').split(',')[0]
+            else
+                field = temp[0].trim()
+            let op = 'equal'
+            values.forEach(v => {
+                let rule = {}
+                rule.field = field
+                rule.operator = op
+                rule.value = v
+                rules[y].rules.push(rule)
+            });
+        }
+        else if(rules[y].toString().includes(' <> ', 0)){
+            let temp = rules[y].split(' <> ')
+            rules[y] = {}
+            if(temp[0].trim().includes('COALESCE',0))
+                rules[y].field = temp[0].trim().replace('COALESCE(','coalesce_').split(',')[0]
+            else
+                rules[y].field = temp[0].trim()
+            rules[y].operator = 'not_equal'
+            rules[y].value = temp[1].trim().split("'").join("")
         }
     }
     return rules;
